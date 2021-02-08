609eb97e7245fdfeb050551df16a39973d8999be
jethrinfox <jethrinfox@gmail.com>
2021-02-07 23:16:16 -0300

    fix: 🐛 gifs & styles

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Gif = ({ gif }) => {
-  const { images, title } = gif
   return (
     <div className="max-w-xs">
-      <img className="w-auto h-auto" src={images.downsized_medium.url} alt={title} />
+      <img className="" src={gif.url} alt={gif.title} />
     </div>
-  )
-}
+  );
+};
 
-export default Gif
+export default React.memo(Gif, (prevProps, nextProps) => {
+  return prevProps.gif.id === nextProps.gif.id;
+});
--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Gif from './Gif'
+import React from "react";
+import Gif from "./Gif";
 
 const GifList = ({ gifs }) => {
   return (
-    <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row-dense grid-masonry gap-4">
-      {gifs.map(gif => (
+    <div className="container mx-auto mt-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row-dense grid-masonry gap-4">
+      {gifs.map((gif) => (
         <Gif gif={gif} key={gif.id} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default GifList
+export default GifList;
